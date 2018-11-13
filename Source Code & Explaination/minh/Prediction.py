bf833eb1e00afd48f30f7c92298644342f01a5b0
Ton Nu Minh Chau <minhchau1997208@gmail.com>
2018-11-13 23:50:34 +0700

    update predict 2

--- Source Code & Explaination/minh/Prediction.py
+++ Source Code & Explaination/minh/Prediction.py
@@ -7,14 +7,27 @@ model = load_model('E:/HK7/ML/Pyth/CNN-keras/models/mnistCNN.h5')
 # load data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
+# Reshaping: (batch, height, width, channels)
+test = X_test[0].reshape(1,28,28,1)
+
 # Show
+# print(X_test.shape)
 # plt.imshow(X_test[0])
 # plt.show()
 
-# print(X_test.shape)
+# Show2
+# Mode "L" :greyscale (8-bit pixels, black and white)
+img = Image.open("E:/HK7/ML/Pyth/CNN-keras/test2.png").convert('L')
+img = img.resize((28,28))
+imgArr = np.array(img)
+# print(imgArr.shape)
 
-# # Reshaping: (batch, height, width, channels)
-test = X_test[0].reshape(1,28,28,1)
+# Reshaping: (batch, height, width, channels)
+imgArr = imgArr.reshape(1,imgArr.shape[0],imgArr.shape[1],1)
+
+# print(imgArr)
+# plt.imshow('imgArr')
+# plt.show()
 
 # Predicting the Test set results
 y_pred = model.predict(test)
