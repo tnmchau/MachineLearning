2bad7a696b7ee01d4966e7d70393205ff2fd0dbf
Minh Huynh Kien <minhhuynh1703@gmail.com>
2018-12-04 01:27:43 +0700

    no message

--- Báo Cáo Cuối Kỳ(5_12_2018)/SourceCode/Prediction.py
+++ Báo Cáo Cuối Kỳ(5_12_2018)/SourceCode/Prediction.py
@@ -3,31 +3,29 @@ from keras.models import load_model
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
-
+###################################################################### Load model 
 model = load_model('E:\Research\Semester 7\Machine Learning\Git\Source Code & Explaination\minh\mnistCNN.h5')
-# load data
+###################################################################### Load data
+#--------------------------data from MMIST
 #(X_train, y_train), (X_test, y_test) = mnist.load_data()
 #test = X_test[0].reshape(1,28,28,1)
-
 # Show
 # print(X_test.shape)
 # plt.imshow(X_test[0])
 # plt.show()
 #--------------------------------------------------C2
-# Show2
+###################################################################### data tự chuẩn bị 
 # Mode "L" :greyscale (8-bit pixels, black and white)
 img = Image.open("E:/Research/Semester 7/Machine Learning/Git/Source Code & Explaination/minh/test2.png").convert('L')
 img = img.resize((28,28))
 imgArr = np.array(img)
 # print(imgArr.shape)
-
-imgArr = imgArr.reshape(1,imgArr.shape[0],imgArr.shape[1],1)
-
+imgArr = imgArr.reshape(1,imgArr.shape[0],imgArr.shape[1],1)# shape that CNN expects is a 4D array (batch, height, width, channels) <batch: số samples>
 # print(imgArr)
 # plt.imshow('imgArr')
 # plt.show()
 
-# Predicting the Test set results
+####################################################################### Predicting the Test set results
 y_pred = model.predict(imgArr)
 print("Predict results: \n", y_pred[0])
 print("Predict label: \n", np.argmax(y_pred[0]))
