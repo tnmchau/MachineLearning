1c5ef18bf7b2f5b3fce176f7e4b735d59886714f
Ton Nu Minh Chau <minhchau1997208@gmail.com>
2018-12-04 01:43:49 +0700

    update

--- Bao Cao Cuoi Ky(5_12_2018)/SourceCode/CNN_inTensorFlow_withKeras.py
+++ Bao Cao Cuoi Ky(5_12_2018)/SourceCode/CNN_inTensorFlow_withKeras.py
@@ -76,9 +76,25 @@ history= model.fit(X_train, y_train, epochs=10, batch_size=128, validation_data=
 # Save the model
 model.save('D:\Research\Semester 7\Machine Learning\Git\Source Code & Explaination\minh\mnistCNN.h5')
 # Final evaluation of the model
+print("Evaluate:")
 metrics = model.evaluate(X_test, y_test)
 print("Metrics(Test loss & Test Accuracy): ")
 print(metrics)
 
-plot_model(model, to_file='model.png')
-SVG(model_to_dot(model).create(prog='dot', format='svg'))
+# Plot training & validation accuracy values
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
